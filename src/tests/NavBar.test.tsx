cb6dda824c40b1b52f308d4a3e139ea8815402b5
jame <james@realja.me>
2022-04-07 22:30:59 -0400

    Remove useless test files

--- src/tests/NavBar.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import NavBar from "../components/NavBar";
-import Home from "../Home";
-
-describe("Nav bar", () => {
-	//todo
-});
