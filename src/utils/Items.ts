46a2567cbd7bd1a3fb10b8580fc697b9c23db4d7
jame <james@realja.me>
2022-04-11 14:05:00 -0400

    Quick fix to make assets work on gh-pages probably?

--- src/utils/Items.ts
+++ src/utils/Items.ts
@@ -3,27 +3,28 @@ import { ShopItem } from "./Types";
 const items = [
 	{
 		name: "Hershey's Milk Chocolate Bar",
-		image: "../assets/hersheys.png",
+		image:
+			"https://atomicchocolate.github.io/shopping-cart/assets/hersheys.png",
 		price: 0.99,
 	},
 	{
 		name: "Reese's Peanut Butter Cups",
-		image: "../assets/reeses.png",
+		image: "https://atomicchocolate.github.io/shopping-cart/assets/reeses.png",
 		price: 0.99,
 	},
 	{
 		name: "Kit Kat",
-		image: "../assets/kitkat.png",
+		image: "https://atomicchocolate.github.io/shopping-cart/assets/kitkat.png",
 		price: 0.99,
 	},
 	{
 		name: "Twix",
-		image: "../assets/twix.png",
+		image: "https://atomicchocolate.github.io/shopping-cart/assets/twix.png",
 		price: 0.99,
 	},
 	{
 		name: "Hershey's Kisses",
-		image: "../assets/kisses.png",
+		image: "https://atomicchocolate.github.io/shopping-cart/assets/kisses.png",
 		price: 3.79,
 	},
 ] as ShopItem[];
